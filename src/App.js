f09b9116cceb722cb175b08c833647aa7bf48d1e
GiorgiGogsadze <gogsadze1201@gmail.com>
2024-01-22 13:50:16 +0400

    version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [isOpen, setisOpen] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Close setisOpen={setisOpen} />
+      {isOpen ? <Steps /> : null}
+    </>
+  );
+}
+
+function Steps() {
+  const [step, setStep] = useState(1);
+  return (
+    <div className="steps">
+      <Numbers step={step} />
+      <Message step={step} />
+      <Buttons setStep={setStep} />
     </div>
   );
 }
 
-export default App;
+function Close({ setisOpen }) {
+  const handleClick = () => {
+    setisOpen((d) => !d);
+  };
+  return (
+    <button className="close" onClick={handleClick}>
+      &times;
+    </button>
+  );
+}
+
+function Numbers({ step }) {
+  return (
+    <div className="numbers">
+      {messages.map((_, i) => (
+        <Step i={i + 1} active={step > i} key={i + 1} />
+      ))}
+    </div>
+  );
+}
+function Step({ i, active }) {
+  return <div className={`step-${i} ${active ? "active" : ""}`}>{i}</div>;
+}
+
+function Message({ step }) {
+  return (
+    <p className="message">
+      Step {step}: {messages[step - 1]}
+    </p>
+  );
+}
+
+function Buttons({ setStep }) {
+  const loop = (n, a) => {
+    return a % n === 0 ? n : a % n;
+  };
+  const handlePrevious = () => {
+    setStep((a) => loop(messages.length, a - 1));
+  };
+  const handleNext = () => {
+    setStep((a) => loop(messages.length, a + 1));
+  };
+  return (
+    <div className="buttons">
+      <button className="previous" onClick={handlePrevious}>
+        Previous
+      </button>
+      <button className="next" onClick={handleNext}>
+        Next
+      </button>
+    </div>
+  );
+}
